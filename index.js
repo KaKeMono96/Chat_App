7b162728b466244ea3ddb4dd9d3d32cc59659693
lonelonec <ayeayeaung96@gmail.com>
2024-03-10 21:30:52 +0630

    client

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-let express = require('express')
+let express = require('express');
+let socket = require('socket.io')
+
+
+// app setup
+let app =  express();
+
+// server setup
+let server = app.listen(4000, () => {
+    console.log("project is running on localhost:4000");
+});
+
+
+// route setup
+app.get("/", (res, req) => {
+    req.sendFile(__dirname + "/public/index.html");
+})
+
+// socket setup
+
+let io=socket(server)
+io.on('connection',(socket)=>{
+    console.log('socket connection connected');
+})
+
